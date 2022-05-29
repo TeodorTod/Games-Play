9429913a335be5ceec60b895a162e9aa05259e9f
Artos Web <teodor.at.tod@gmail.com>
2022-05-29 12:50:15 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import CreateGame from "./components/CreateGame";
+import GameCatalog from "./components/GameCatalog";
 import Header from "./components/Header";
 import WelcomeWorld from "./components/WelcomeWorld";
 
 
 function App() {
+    const routes = {
+        '/home': WelcomeWorld,
+        '/games': GameCatalog,
+        '/create-game': CreateGame
+    };
+
   return (
     <div id="box">
 
