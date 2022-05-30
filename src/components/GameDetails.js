f9c39abd9c70d2715202512ced6d50faa78c4296
Artos Web <teodor.at.tod@gmail.com>
2022-05-30 22:29:32 +0300

    create game datails services

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,6 +1,18 @@
+import { useState, useEffect } from "react";
+
+import * as gameService from'../services/gameService';
+
 const GameDetails = ({
     id
 }) => {
+    const [game, setGame] = useState({});
+
+    useEffect(() => {
+        gameService.getOne(id)
+          .then(result => {
+              setGame(result);
+          })
+    }, []);
 
     return (
         <section id="game-details">
@@ -8,17 +20,14 @@ const GameDetails = ({
         <div className="info-section">
 
             <div className="game-header">
-                <img className="game-img" src="images/MineCraft.png" />
-                <h1>Bright</h1>
-                <span className="levels">MaxLevel: 4</span>
-                <p className="type">Action, Crime, Fantasy</p>
+                <img className="game-img" src={game.imageUrl} />
+                <h1>{game.title}</h1>
+                <span className="levels">MaxLevel: {game.maxLevel}</span>
+                <p className="type">{game.category}</p>
             </div>
 
             <p className="text">
-                Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
-                with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
-                creatures live side by side with humans. A human cop is forced
-                to work with an Orc to find a weapon everyone is prepared to kill for.
+                {game.summary}
             </p>
 
             <div className="details-comments">
