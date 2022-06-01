7f2a4f593050675b872b548183ae879ec64ee704
Artos Web <teodor.at.tod@gmail.com>
2022-06-01 21:36:20 +0300

    intergate code split

--- src/components/GameCatalog/GameCard.js
+++ src/components/GameCatalog/GameCard.js
@@ -1,4 +1,6 @@
+import { lazy } from "react";
 import { Link } from "react-router-dom";
+//const raitongCalculator = lazy(() => import('../../utils/ratingCalculator'));
 
 const GameCard = ({
     game
@@ -10,6 +12,7 @@ const GameCard = ({
             <img src={game.imageUrl} />
             <h6>{game.category}</h6>
             <h2>{game.title}</h2>
+            {/*<p>Raiting {raitongCalculator()}</p>*/}
             <Link to={`/games/${game._id}`} className="details-button">Details</Link>
         </div>
     </div>
--- src/components/GameCatalog/GameCatalog.js
+++ src/components/GameCatalog/GameCatalog.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-import GameCard from "./GameCard";
+import { useEffect, useState, lazy, Suspense } from "react";
+// import GameCard from "./GameCard";
 import * as gameService from '../../services/gameService';
+const GameCard = lazy(() => import('./GameCard'));
 
 const GameCatalog = ({
     naviagtionChangeHandler
@@ -18,11 +19,12 @@ const [games, setGames] = useState([]);
     return (
         <section id="catalog-page">
         <h1>All Games</h1>
-
-        { games.length > 0 
-            ? games.map(x => <GameCard key={x._id} game={x} naviagtionChangeHandler={naviagtionChangeHandler}/>)
-            :    <h3 className="no-articles">No games yet</h3>
-        }
+        <Suspense fallback={<p>Loading...</p>}>
+            { games.length > 0 
+                ? games.map(x => <GameCard key={x._id} game={x} naviagtionChangeHandler={naviagtionChangeHandler}/>)
+                :    <h3 className="no-articles">No games yet</h3>
+            }
+        </Suspense>
 
     </section>
 )
